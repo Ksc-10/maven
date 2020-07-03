1a74aeaf39007aa7dd6efed93652560cbf34c81f
ksc--10 <809759736@qq.com>
2020-07-03 18:52:14 +0800

    helloworld02

--- untitled/src/com/itheima/Demo.java
+++ untitled/src/com/itheima/Demo.java
@@ -3,5 +3,8 @@ package com.itheima;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld01");
+        System.out.println("HelloWorld02");
     }
 }
